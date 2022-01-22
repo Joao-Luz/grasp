874cb4644fa26be6830b5627d2878e62c1af56ce
Joao-Luz <jlllima5@gmail.com>
2022-01-22 20:55:39 -0300

    Updated the grasp procedure

--- grasp/grasp.c
+++ grasp/grasp.c
@@ -1,9 +1,10 @@
 #include <stdlib.h>
 #include <stdio.h>
+#include <math.h>
 #include "grasp.h"
 
 float argmin(float* elements, int n, bool* index) {
-	float min = 999999;
+	float min = INFINITY;
 	for (int i = 0; i < n; i++) {
 		if (elements[i] < min && index[i]) min = elements[i];
 	}
@@ -11,7 +12,7 @@ float argmin(float* elements, int n, bool* index) {
 }
 
 float argmax(float* elements, int n, bool* index) {
-	float max = -999999;
+	float max = -INFINITY;
 	for (int i = 0; i < n; i++) {
 		if (elements[i] > max && index[i]) max = elements[i];
 	}
@@ -20,12 +21,12 @@ float argmax(float* elements, int n, bool* index) {
 
 static void construct(grasp* g, void* elements, const int n_elements, int* solution, int* n_solution) {
 	// construction
-	// compute incremental costs
-	float costs[n_elements];
-	g->compute_costs(g, elements, n_elements, costs);
-
 	// initialize empty solution
 	*n_solution = 0;
+	// compute incremental costs
+	float costs[n_elements];
+	g->compute_costs(g, elements, n_elements, solution, *n_solution, costs);
+
 	
 	// initialize candidate list
 	bool candidates[n_elements];
@@ -46,18 +47,17 @@ static void construct(grasp* g, void* elements, const int n_elements, int* solut
 			if (candidates[k] == false) continue;
 			if (g->max) {
 				if (costs[k] >= base_cost) rcl[n_rcl++] = k;
-			}
-			else
+			} else {
 				if (costs[k] <= base_cost) rcl[n_rcl++] = k;
+			}
 		}
-
 		// select random element from rcl
 		int element = rcl[rand() % n_rcl];
 		solution[(*n_solution)++] = element;
 
 		// update candidates and incremental costs
-		g->compute_costs(g, elements, n_elements, costs);
 		g->update_candidates(g, elements, n_elements, solution, *n_solution, candidates);
+		g->compute_costs(g, elements, n_elements, solution, *n_solution, costs);
 		n_candidates = 0;
 		for (int k = 0; k < n_elements; k++) if (candidates[k] == true) n_candidates += 1; 
 	}
@@ -66,10 +66,13 @@ static void construct(grasp* g, void* elements, const int n_elements, int* solut
 void grasp_run(grasp* g, void* elements, const int n_elements, int* best_solution, int* n_best_solution) {
 	
 	int solution[n_elements];
-	int n_solution;
+	int n_solution = 0;
 	for (int i = 0; i < g->iterations; i++){
 		construct(g, elements, n_elements, solution, &n_solution);
+
+		g->post_construction(g, elements, n_elements, solution, n_solution);
 		g->local_search(g, elements, n_elements, solution, &n_solution);
-		g->compare_solutions(g, elements, solution, n_solution, best_solution, n_best_solution);
+		bool first = i == 0 ? true : false;
+		g->compare_solutions(g, elements, solution, n_solution, best_solution, n_best_solution, first);
 	}	
 }
--- grasp/grasp.h
+++ grasp/grasp.h
@@ -4,15 +4,17 @@
 typedef enum { false, true } bool;
 typedef struct grasp grasp;
 
-typedef void (*grasp_cost) (grasp* g, void* elements, int n_elements, float* costs);
+typedef void (*grasp_cost) (grasp* g, void* elements, int n_elements, int* solution, int n_solution, float* costs);
 typedef void (*grasp_candidates) (grasp* g, void* elements, int n_elements, int* solution, int n_solution, bool* candidates);
-typedef void (*grasp_compare) (grasp* g, void* elements, int* solution, int n_solution, int* best_solution, int* n_best_solution);
+typedef void (*grasp_compare) (grasp* g, void* elements, int* solution, int n_solution, int* best_solution, int* n_best_solution, bool first_solution);
 typedef void (*grasp_search) (grasp* g, void* elements, int n_elements, int* solution, int* n_solution);
+typedef void (*grasp_post_construction) (grasp* g, void* elements, int n_elements, int* solution, int n_solution);
 
 // This is the struct for and instance of GRASP
 // iterarions - number of iterations
 // alpha - the parameter alpha
 // max - boolean to indicate if the problem should find the maximum (true) or mininum (false) cost
+// data - any data that might be usefull to solve the problem
 // compute_costs - function to evaluate the incremental cost of each element in the candidate list
 // update_candidates - function to update the candidate list at each iteration of construction
 // compare_solutions - the function to compare two solutions 
@@ -21,11 +23,12 @@ struct grasp {
 	int iterations;
 	float alpha;
 	bool max;
-	void* problem;
+	void* data;
 	grasp_cost compute_costs;
 	grasp_candidates update_candidates;
 	grasp_compare compare_solutions;
 	grasp_search local_search;
+	grasp_post_construction post_construction;
 };
 
 // Run the grasp algorithm for a set of elements
