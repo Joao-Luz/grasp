e74c4b8281d2cfc3b9bc851aa804b7505783f92d
Joao-Luz <jlllima5@gmail.com>
2022-03-03 16:24:07 -0300

    Changed `test.sh` to run and generate `csv`'s for all alphas

--- test/test.sh
+++ test/test.sh
@@ -2,22 +2,22 @@
 
 make > /dev/null
 
-echo "problem,time,cost,optimal"
-
-alpha="$1"
-iter="$2"
-
-for file in cvrp/vrp-A/*.vrp; do
-    for i in 1 2 3 4 5 6 7 8; do
-        output=$( (./grasp_cvrp "$file" "$alpha" "$iter";) 2>&1 )
-        time=$( (echo "$output" | grep 'Time';) 2>&1)
-        time=${time:5:7}
-        cost=$( (echo "$output" | grep 'Cost';) 2>&1)
-        cost=${cost#"Cost "}
-        optimal=$( (cat ${file%.*}.sol | grep "Cost";) 2>&1 )
-        optimal=${optimal#"Cost "}
-        problem=${file##*/}
-        problem=${problem%.*}
-        echo -e "$problem,$time,$cost,$optimal"
+for alpha in "0.2" "0.4" "0.5" "0.6" "0.8"; do
+    echo -e "Alpha = $alpha"
+    echo "problem,time,cost,optimal" > "test/data/tests_$alpha.csv"
+    for file in cvrp/vrp-*/*.vrp; do
+        echo -e "Running $file..."
+        for i in 1 2 3 4 5 6 7 8; do
+            output=$( (./grasp_cvrp "$file" --alpha "$alpha" --iter 200;) 2>&1 )
+            time=$( (echo "$output" | grep 'Time';) 2>&1)
+            time=${time:5:7}
+            cost=$( (echo "$output" | grep 'Cost';) 2>&1)
+            cost=${cost#"Cost "}
+            optimal=$( (cat ${file%.*}.sol | grep "Cost";) 2>&1 )
+            optimal=${optimal#"Cost "}
+            problem=${file##*/}
+            problem=${problem%.*}
+            echo -e "$problem,$time,$cost,$optimal" >> "test/data/tests_$alpha.csv"
+        done
     done
 done
